ae564e2ce1ed4357eb9c795b75d978d4b49143bf
RafalTaraska <paulo.dorosz@gmail.com>
2022-10-23 09:26:05 +0200

    Fix_suma produktów

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,9 +2,9 @@ lista_zakupow = {
     "piekarnia": ["Chleb","Paczek","Bulki"],
     "warzywniak": ["Marchew","Seler","Rukola"]}
 
-
+v = 0
 for key, value in lista_zakupow.items():
     print("Ide do",key.upper(),"i kupuje tam",value)
     ilosc = len(value)
-    
+    v = v + ilosc
 print("W sumie kupiłem",v,"produktow")
