37692f4d1a4e4fc42e412c9e9d57dc053fd4f63f
RafalTaraska <paulo.dorosz@gmail.com>
2022-10-23 09:23:55 +0200

    Add petla for

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,9 @@ lista_zakupow = {
     "piekarnia": ["Chleb","Paczek","Bulki"],
     "warzywniak": ["Marchew","Seler","Rukola"]}
 
+
+for key, value in lista_zakupow.items():
+    print("Ide do",key.upper(),"i kupuje tam",value)
+    ilosc = len(value)
+    
+print("W sumie kupiłem",v,"produktow")
